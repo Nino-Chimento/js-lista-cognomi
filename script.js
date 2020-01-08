ae0b535ee2760cb4be36e818964fe748c1319af8
Nino Chimento <saninoc84@gmail.com>
2020-01-08 12:55:40 +0100

    rimosso ultimo valore indefinito

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ console.log(listaNomi);
 // stampa la lista ordinata alfabeticamente (in html ul > li).
 listaNomi.sort();
 console.log(listaNomi);
-for (var i = 0; i <= listaNomi.length; i++) {
+for (var i = 0; i <= listaNomi.length - 1; i++) {
 
   document.getElementById('lista-nomi').innerHTML += "<li>" + listaNomi[i] + "</li>"
 }
-document.getElementById('lista-nomi').innerHTML +=
+
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
 console.log(listaNomi.indexOf(nomeUtente ));
