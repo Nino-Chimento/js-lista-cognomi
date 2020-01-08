eca6e311658e37930c9a9eb269b5eb95207383fe
Nino Chimento <saninoc84@gmail.com>
2020-01-08 12:39:33 +0100

    stampo la lista in ordine alfabetico

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ var nomeUtente = prompt("Inserisci il tuo nome");
 listaNomi = ["marco","andrea","fabio","ludovico","giovanni"];
 listaNomi.push(nomeUtente);
 console.log(listaNomi);
+
 // stampa la lista ordinata alfabeticamente (in html ul > li).
+listaNomi.sort();
+console.log(listaNomi);
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
