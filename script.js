e5a8c99b8e36599b95e960b4fa977e146d38af69
Nino Chimento <saninoc84@gmail.com>
2020-01-08 12:36:23 +0100

    creazione imput e inserimento in lista nomi

---
+++ script.js
@@ -0,0 +1,8 @@
+// Chiedi all’utente il cognome,
+var nomeUtente = prompt("Inserisci il tuo nome");
+// inseriscilo in un array con altri cognomi,
+listaNomi = ["marco","andrea","fabio","ludovico","giovanni"];
+listaNomi.push(nomeUtente);
+console.log(listaNomi);
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
